618d15e4d32d719d0c95af1f6b511090189908a3
stpotato <sctom@ucla.edu>
2017-11-13 19:56:35 -0800

    parse

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // Node server
 const express = require('express');
 const path = require('path');
-//const parser = require('/parse_commands');
+const parser = require('./parse_commands');
 
 const app = express();
 const port = 3000;
@@ -12,51 +12,61 @@ app.get('/', (request, response) => {
 });
 
 app.get('/command', (request, response) => {
-   const command = parser.parseCommands(request.body);
-   switch (command) {
-      case 'coffee':
-      {
-         return getCoffee((res, er) => {
-            if (er) {
-               console.log("Error while getting coffee", er);
-            }
-            else {
-               console.log("Got Coffee", res);
-               response.send("Got Coffee!");
-            }
-         });
-         break;
+
+   // parse() expects a file path to audio: 
+   // file type: .flac, bit resoultion: 16 bit, 
+   // sampling rate: 16000 hertz, audio channel: mono
+   // as an example: "resources/makeMeCoffee.flac"
+   const getParse = parser.parse(request.body); 
+   getParse.then(function(result){
+      const command = result.action;
+      switch (command) {
+         case 'coffee':
+         {
+            return getCoffee((res, er) => {
+               if (er) {
+                  console.log("Error while getting coffee", er);
+               }
+               else {
+                  console.log("Got Coffee", res);
+                  response.send("Got Coffee!");
+               }
+            });
+            break;
+         }
+         case 'lights':
+         {
+            return switchLights((res, er) => {
+               if (er) {
+                  console.log("Error while changing lights", er);
+               }
+               else {
+                  console.log("Flipped lights", res);
+                  response.send("Flipped lights!");
+               }
+            });
+            break;
+         }
+         case 'search':
+         {
+            return gSearch((res, er) => {
+               if (er) {
+                  console.log("Error while google searching", er);
+               }
+               else {
+                  console.log("Searching", res);
+                  response.send("Searching!");
+               }
+            });
+            break;
+         }
+         default:
+            console.log("Unidentified command:", command);
+            response.send("Could not understand command", command);
       }
-      case 'lights':
-      {
-         return switchLights((res, er) => {
-            if (er) {
-               console.log("Error while changing lights", er);
-            }
-            else {
-               console.log("Flipped lights", res);
-               response.send("Flipped lights!");
-            }
-         });
-         break;
-      }
-      case 'search':
-      {
-         return gSearch((res, er) => {
-            if (er) {
-               console.log("Error while google searching", er);
-            }
-            else {
-               console.log("Searching", res);
-               response.send("Searching!");
-            }
-         });
-         break;
-      }
-      default:
-         console.log("Unidentified command:", command);
-         response.send("Could not understand command", command);
-   }
+   }).catch(err => {
+      console.log("ERROR: ", err);
+   })
 });
 
 app.listen(port, (err) => {
--- api/parse_commands.js
+++ api/parse_commands.js
@@ -13,78 +13,64 @@ function getAction(command) {
     return result;
 }
 
+exports.parse = function (audioFile) {
 
-function parseCommands(audioFile) {
+	// Imports the Google Cloud client library
+	const speech = require('@google-cloud/speech');
+	const fs = require('fs');
 
-// Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
+	// Your Google Cloud Platform project ID
+	const projectId = 'rugged-sunbeam-185822';
 
-// Your Google Cloud Platform project ID
-const projectId = 'rugged-sunbeam-185822';
+	// set environment variable
+	process.env['GOOGLE_APPLICATION_CREDENTIALS'] = "googleSpeechCredentials.json";
 
-// set environment variable
-process.env['GOOGLE_APPLICATION_CREDENTIALS'] = "googleSpeechCredentials.json";
+	// Creates a client
+	const client = new speech.SpeechClient({ });
 
-// Creates a client
-const client = new speech.SpeechClient(
-{  // projectId: projectId,
-}
-);
+	// const fileName = 'resources/makeMeCoffee.flac';
+	// const fileName = 'resources/searchWeather.flac';
+	// const fileName = 'resources/turnOnLights.flac';
+	const fileName = audioFile;
 
-// The name of the audio file to transcribe
-// const fileName = './resources/makeMeCoffee.flac';
-// const fileName = './resources/searchWeather.flac';
-// const fileName = './resources/turnOnLights.flac';
-const fileName = audioFile;
+	// Reads a local audio file and converts it to base64
+	const file = fs.readFileSync(fileName);
+	const audioBytes = file.toString('base64');
 
+	// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+	// file type: .flac
+	// bir resoultion: 16 bit
+	// sampling rate: 16000 hertz
+	// audio channel: mono
+	const audio = {
+	  content: audioBytes,
+	};
+	const config = {
+	  encoding: 'FLAC',
+	  sampleRateHertz: 16000,
+	  languageCode: 'en-US',
+	};
+	const request = {
+	  audio: audio,
+	  config: config,
+	};
+	let getParse = function (request) {
+		return new Promise( function (resolve, reject) { 
+		client
+		  .recognize(request)
+		  .then(data => {
+		    const response = data[0];
+		    const transcription = response.results
+		      .map(result => result.alternatives[0].transcript)
+		      .join('\n');
+		    resolve (getAction(transcription));
+		  })
+		  .catch(err => {
+		    console.error('ERROR:', err);
+		  });
+	})
+	};
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
-
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-// file type: .flac
-// bir resoultion: 16 bit
-// sampling rate: 16000 hertz
-// audio channel: mono
-
-const audio = {
-  content: audioBytes,
-};
-
-const config = {
-  encoding: 'FLAC',
-  sampleRateHertz: 16000,
-  languageCode: 'en-US',
-
-  
-};
-const request = {
-  audio: audio,
-  config: config,
-};
-
-// Detects speech in the audio file
-
-client
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    // text = Object.assign({}, response.results);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-    console.log("Action:", getAction(transcription));
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-
-  
+	return getParse(request);
 
 }
-
-
-module.exports = parseCommands;
